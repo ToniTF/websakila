80a0b29e9c3c06295882633919d7ba9e6fcbb10d
Tonitf <subcero00@hotmail.com>
2025-02-13 20:05:11 +0100

    cambia a localhost

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
 document.getElementById("peliculas").addEventListener("click", function () {
     document.getElementById("contenido").hidden = false;
     document.getElementById("form_new_actor").hidden = true;
-    fetch("http://192.168.100.166/apisakila/peliculas/all")
-        .then(response => response.json())
-        .then(data => {
-            let output = "<h2>Peliculas</h2>";
-            data.forEach(function (pelicula) {
-                output += `
+    fetch("http://localhost/apisakila/peliculas/all", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0dS1hcGkuY29tIiwiYXVkIjoidHUtYXBpLmNvbSIsImlhdCI6MTczOTQ3MTgyMCwiZXhwIjoxNzM5NDc1NDIwLCJ1c2VyX2lkIjoxfQ.cTJFlI7_nhiqyaeJ3NNzrJRKV1u_zq4CrYtSMobJF3s"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        let output = "<h2>Peliculas</h2>";
+        data.forEach(function (pelicula) {
+            output += `
                 <div>
                     <h3>${pelicula.title}</h3>
                     <p>${pelicula.description}</p>
                 </div>
             `;
-            });
-            document.getElementById("contenido").innerHTML = output;
         });
+        document.getElementById("contenido").innerHTML = output;
+    })
+    .catch(error => console.error('Error:', error));
 });
 
 document.getElementById("new_actor").addEventListener("click", function () {
@@ -36,5 +43,6 @@ document.getElementById("save_actor").addEventListener("click", function () {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-    });
+    })
+    .catch(error => console.error('Error:', error));
 });
